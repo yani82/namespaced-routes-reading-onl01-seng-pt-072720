2f2f0033947fd6f02f703bbd6fa82ed85ab2357e
yani82 <yani.iman1@gmail.com>
2021-01-03 12:27:02 -0500

    commit tree 79289ca5f055d5e7835101fa91f33165ebb3202e

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   resources :posts, only: %i[index show new create edit update]
 
   get '/admin/stats', to: 'stats#index'
+  get '/admin/authors/new', to: 'authors#new'
+  get '/admin/authors/delete', to: 'authors#delete'
+  get '/admin/authors/create', to: 'authors#create'
+  get '/admin/comments/moderate', to: 'comments#moderate'
 
   root 'posts#index'
 end
